000440a161861fa5e5bb95789fbf7d0b76fd249d
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-09-17 11:25:43 -0400

    Add XSS escape function and display error validation with jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -32,32 +32,38 @@
 
 //returns tweet <article> element containing the entire HTML structure of the tweet
 const createTweetElement = function (tweetData) {
-  console.log("inside createTweetElement. tweetData:" + tweetData);
-  let tweet = $(`<article class='tweet'>
+  const { user, content, created_at } = tweetData;
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  let $tweet = `<article class='tweet'>
       <header>
         <div class='user-info'>
-          <img src="${tweetData.user.avatars}">
-          <p>${tweetData.user.name}</p>
+          <img src="${user.avatars}">
+          <p>${user.name}</p>
         </div>
-        <p class='username'>${tweetData.user.handle}</p>
+        <p class='username'>${user.handle}</p>
       </header>
-      <p class="tweet-display">${tweetData.content.text}</p>
+      <p class="tweet-display">${escape(content.text)}</p>
       <footer>
-        <p>${timeago.format(tweetData.created_at)}</p>
+        <p>${timeago.format(created_at)}</p>
         <div class='tweet-icons'>
           <i class="fas fa-flag"></i>
           <i class="fas fa-retweet"></i>
           <i class="fas fa-heart"></i>
         </div>
       </footer>
-    </article>`)
-  return tweet;
+    </article>`;
+  return $tweet;
 };
 
 const renderTweets = function (tweets) {
-  console.log("in the render tweet");
+
   for (let tweet of tweets) {       // loops through tweets
-    console.log("inside individual tweet");
+
     const $tweet = createTweetElement(tweet)// calls createTweetElement for each tweet
     $('#tweets-container').prepend($tweet);  // takes return value and appends it to the tweets container
 
@@ -70,12 +76,16 @@ $(document).ready(function () {
 
   $('form').submit(function (event) { //form submit using jQuery
     event.preventDefault();
+    $('.counter-exceed').hide();
+    $('.empty-tweet').hide();
     console.log("text-tweet:" + $(this).find('#tweet-text').val());
     console.log("text-tweet using html:" + document.getElementById("tweet-text").value);
     if ($(this).find('.counter').val() < 0) {
-      return alert('Tweet is too long!');
+      //return alert('Tweet is too long!');
+      $('.counter-exceed').slideDown();
     } else if (!$('#tweet-text').val()) {
-      return alert('Please enter the tweet');
+      //return alert('Please enter the tweet');
+      $('.empty-tweet').slideDown();
     }
     const data = $(this).serialize();
     $.post('/tweets', data)
